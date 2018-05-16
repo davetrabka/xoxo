0cd5d519ecac784674589ec63778014bebf77622
Sean Lee <seanslee92@gmail.com>
2018-05-16 12:28:10 -0400

    first commit

--- game/index.js
+++ game/index.js
@@ -1,6 +1,18 @@
 import {Map} from 'immutable'
+// import { createStore } from "redux"
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+export default function reducer(state = { turn: 'X', board: Map() }, action) {
+  switch (action.type) {
+    case 'START':
+      return {}
+    case 'MOVE'
+      return 
+    default: 
+      return state
+  }
+}
+
+const move = (turn, coor) => {
+  const nextTurn = turn === 'X' ? 'O' : 'X'
+  return {type: 'MOVE', turn: nextTurn}
 }
